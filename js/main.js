c2baec9cf336ca8dfed4fa019905682494c5de2a
Denis <denistrator@yandex.ua>
2016-08-25 21:41:02 +0300

    end with makeup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 $( document ).ready(function() {
-    $('.main_slider_major_img').height(($('.main_slider').height()) - 20); // 20 is parrent's padding
+    // $('.main_slider_major_img').height(($('.main_slider').height()) - 20); // 20 is parrent's padding
 });	
